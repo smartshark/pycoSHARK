6b9949d4ae1b5315bff083594bdbdfe901ad7a72
atx <atx@drecks-provider.de>
2016-12-19 14:58:59 +0100

    pycoshark as python library

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+import pycoshark
+
+setup(
+    name='pycoSHARK',
+    version=pycoshark.__version__,
+    description='Basic MongoDB Models for smartSHARK.',
+    install_requires=['mongoengine', 'pymongo'],
+    author='ftrautsch',
+    author_email='fabian.trautsch@uni-goettingen.de',
+    url='https://github.com/smartshark/pycoSHARK',
+    download_url='https://github.com/smartshark/pycoSHARK/tarball/master',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache2.0 License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
