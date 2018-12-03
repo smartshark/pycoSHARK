eb5beb1471e3950606f792e7f835d9a183b94d1d
alexander.linne <alexander.linne@stud.uni-goettingen.de>
2018-12-03 15:16:16 +0100

    add abstraction to manage MongoDB connections across subprocesses

--- pycoshark/utils.py
+++ pycoshark/utils.py
@@ -1,6 +1,8 @@
 import argparse
 import hashlib
 
+from mongoengine import connect, connection, Document
+
 from pycoshark.mongomodels import *
 
 def is_authentication_enabled(db_user, db_password):
@@ -31,6 +33,32 @@ def create_mongodb_uri_string(db_user, db_password, db_hostname, db_port, db_aut
 
     return uri
 
+class MongoDBConnection:
+    def __init__(self, hostname, port, user, password,
+        authentication, ssl_enabled, database):
+        self.hostname = hostname
+        self.port = port
+        self.user = user
+        self.password = password
+        self.authentication = authentication
+        self.ssl_enabled = ssl_enabled
+        self.database = database
+        self.uri = create_mongodb_uri_string(
+            user, password, hostname, port, authentication, ssl_enabled)
+
+    def _reset_connection_cache(self):
+        connection._connections = {}
+        connection._connection_settings ={}
+        connection._dbs = {}
+        for document_class in Document.__subclasses__():
+            document_class._collection = None
+
+    def connect(self):
+        connect(self.database, host=self.uri)
+
+    def reconnect(self):
+        self._reset_connection_cache()
+        self.connect()
 
 def get_code_entity_state_identifier(long_name, commit_id, file_id):
     """
