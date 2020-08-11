85af40bd953dd2e16ff435812b3d7a63594da3bd
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2020-08-11 12:59:18 +0200

    fix docu links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -154,7 +154,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/SmartSHARK_read_the_docs_logo.png'
+html_logo = 'pycoSHARK/images/SmartSHARK_read_the_docs_logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -188,7 +188,7 @@ html_logo = 'images/SmartSHARK_read_the_docs_logo.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['pycoSHARK/_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
