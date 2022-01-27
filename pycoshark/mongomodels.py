34b93c9a5ed0586d90778a9c4da4b0a3be6df8b7
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2022-01-27 15:50:05 +0100

    fix typo

--- pycoshark/mongomodels.py
+++ pycoshark/mongomodels.py
@@ -868,7 +868,7 @@ class FileAction(Document):
     :property lines_deleted: (:class:`~mongoengine.fields.IntField`)  number of lines deleted
     :property is_binary: (:class:`~mongoengine.fields.BooleanField`)  shows, if file is binary
     :property old_file_id: (:class:`~mongoengine.fields.ObjectIdField`) :class:`~pycoshark.mongomodels.File` id of the old file (if it was moved, none otherwise)
-    :property inducig: (:class:`~mongoengine.fields.ListField`) list of :class:`~mongoengine.fields.DictField` contains change_file_action_id, label and lines of the changing commit to the inducing commit in this FileAction.
+    :property induces: (:class:`~mongoengine.fields.ListField`) list of :class:`~mongoengine.fields.DictField` contains change_file_action_id, label and lines of the changing commit to the inducing commit in this FileAction.
     """
 
     meta = {
